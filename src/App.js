0cc1e8d326756e042959d42dd5f260e4bb3a5400
Derek A <dadelman87@gmail.com>
2022-07-19 21:23:07 -0700

    created leaflet map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>It all begins here...</h1>
-    </div>
-  );
+    return (
+        <MapContainer center={[47.61715123413518, -122.15363276388577]} zoom={13} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[47.61715123413518, -122.15363276388577]}>
+                <Popup>
+                    148th Ave & 8th St
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
 }
 
 export default App;
