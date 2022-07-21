a03dc76b2bc2ccd89f8827e92ec56d3ddd8e0b3d
Derek A <dadelman87@gmail.com>
2022-07-20 19:31:45 -0700

    starting authentication page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Button, Container, Form, Header, Icon, Segment, Label } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+        <Button>
+            Click this shit
+        </Button>
+        <div class="ui animated button" tabindex="0">
+            <div class="visible content">Next</div>
+            <div class="hidden content">
+                <i class="right arrow icon"></i>
+            </div>
+        </div>
+        <div class="ui labeled button" tabindex="0">
+            <div class="ui red button">
+                <i class="heart icon"></i> Like
+            </div>
+            <a class="ui basic red left pointing label">
+                1,048
+            </a>
+        </div>
+
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
